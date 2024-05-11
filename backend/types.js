dd636e3478871cf2368148dc26f25f52d757e410
shivkumarojha <ojhashivkumar88@gmail.com>
2024-05-11 14:13:28 +0530

    fix some type syntax issues

--- backend/types.js
+++ backend/types.js
@@ -1,25 +1,25 @@
 const zod = require("zod");
 
-// Zod Schema for todo 
+// Zod Schema for todo
 const createTodoSchema = zod.object({
   title: zod.string(),
-  description: zod.string()
+  description: zod.string(),
 });
 
 // Zod Schema for completed todo Items
 const completeTodoSchema = zod.object({
-    id: zod.string()
-})
+  id: zod.string(),
+});
 
 // Zod schema for update todo
 const updateTodoSchema = zod.object({
-    id: zod.string(),
-    title: string,
-    descriptilon: string
-})
+  id: zod.string(),
+  title: zod.string(),
+  descriptilon: zod.string(),
+});
 
 module.exports = {
-    createTodo,
-    updateTodo
-}
-
+  createTodoSchema,
+  updateTodoSchema,
+  completeTodoSchema
+};
