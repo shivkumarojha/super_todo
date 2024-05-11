5091fe1c371880d6cace63dabdfbc0997a3ae0ea
shivkumarojha <ojhashivkumar88@gmail.com>
2024-05-11 16:53:33 +0530

    frontend init

---
+++ frontend/components/CreateTodo.jsx
@@ -0,0 +1,9 @@
+export default function CreateTodo() {
+  return (
+    <div>
+      <input type="text" placeholder="Title" /> <br /> <br />
+      <input type="text" placeholder="Description" /> <br /> <br />
+      <button>Add a todo</button>
+    </div>
+  );
+}
---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import CreateTodo from "../components/CreateTodo";
+function App() {
+  return (
+    <>
+      <h2>Tasks</h2>
+      <CreateTodo />
+    </>
+  );
+}
+
+export default App;
