e01739d220e88825348e48472df759c8d3d0ef02
shivkumarojha <ojhashivkumar88@gmail.com>
2024-05-11 13:51:10 +0530

    fixed types and added update type schema for todo app

--- backend/types.js
+++ backend/types.js
@@ -1,16 +1,22 @@
 const zod = require("zod");
 
 // Zod Schema for todo 
-const createTodo = zod.object({
+const createTodoSchema = zod.object({
   title: zod.string(),
   description: zod.string()
 });
 
 // Zod Schema for completed todo Items
-const updateTodo = zod.object({
+const completeTodoSchema = zod.object({
     id: zod.string()
 })
 
+// Zod schema for update todo
+const updateTodoSchema = zod.object({
+    id: zod.string(),
+    title: string,
+    descriptilon: string
+})
 
 module.exports = {
     createTodo,
