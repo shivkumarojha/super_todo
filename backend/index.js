c4d7ff1bace2a8f75750ae903effcc872dea181c
shivkumarojha <ojhashivkumar88@gmail.com>
2024-05-11 14:14:45 +0530

    Get and Post route for adddin a todo and retrieving todos are done

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const PORT = 3000;
 
+// Zod schemas for todo
+const { createTodoSchema, updateTodoSchema } = require("./types");
 const app = express();
+
+const { todo } = require("./db");
 
 // Body Parser
 app.use(express.json());
@@ -12,27 +16,45 @@ app.get("/health", (req, res) => {
 });
 
 // Post Todo
-app.post('/todo', (req, res) {
+app.post("/todo", async (req, res) => {
+  const data = req.body;
+  const parsedData = createTodoSchema.safeParse(data);
 
-})
-     
+  if (!parsedData.success) {
+    res.send(411).json({ message: "You sent wrong inputs" });
+    return;
+  }
+
+  // put it in mongo db
+  await todo.create({
+    title: parsedData.data.title,
+    description: parsedData.data.description,
+    completed: false,
+  });
+
+  res.status(200).json({
+    message: "Todo is created",
+  });
+});
+
 // Get todo
-app.get('/todos', (req, res) => {});
+app.get("/todos", async (req, res) => {
+  const todos = await todo.find();
+  console.log(todos);
+  res.status(200).json({
+    message: "success",
+    todos: todos,
+  });
+});
 
 // update Todo
-app.put('/update-todo', (req, res) => {
-
-})
+app.put("/update-todo", (req, res) => {});
 
 // Delete todo
-app.delete('/delete-todo', (req, res) => {
-
-})
+app.delete("/delete-todo", (req, res) => {});
 
 // Mark Completed
-app.post('/completed',  (req, res) => {
-    
-})
+app.post("/completed", (req, res) => {});
 
 // Run server
 app.listen(PORT, () => {
